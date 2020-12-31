adb706cb4b7ab4124a304105da448d1532047051
Dasol <dsestil@gmail.com>
2020-12-31 15:39:43 +0900

    Bi2Se3surf fix

--- src/WannierGauge/LengthGauge/MovingFrame/GeneralModel/Materials/Bi2Se3surf.h
+++ src/WannierGauge/LengthGauge/MovingFrame/GeneralModel/Materials/Bi2Se3surf.h
@@ -97,10 +97,21 @@ BieSe3surf::BieSe3surf( const libconfig::Setting *params )
     // --> reciprocal : (2*pi/a0, 2*pi/a0/sqrt(3.) ), (0., 4*pi/a0/sqrt(3.))
     // same with Haldane notation (sqrt(3.)*a0 --> a0)
     //double kxMax = pi/sqrt(3.)/a0;  double kyMax = 2.*pi/3./a0;
+
     double kxMax = pi/a0;  double kyMax = 2.*pi/sqrt(3.)/a0;
+
+    // New axis
+    // double kxMax = 2*pi/a0;  double kyMax = sqrt(3.)*pi/a0;
     BZaxis = { 2*kxMax,       0,         0,
                   0,       2*kyMax,      0,
                   0,          0,         0};
+
+
+    // double kxMax = 2*pi/a0;  double kyMax = sqrt(3.)*pi/a0;
+    // BZaxis = { 2*kxMax,       0,         0,
+    //              kxMax,    2*kyMax,      0,
+    //               0,          0,         0};
+    // BZorigin = {-1.5*kxMax, -kyMax, 0};
     BZorigin = {0, 0, 0};
 
     eps = 1.0e-18;
@@ -173,13 +184,28 @@ void BieSe3surf::GenInitialValue(complex *_dmstore, std::array<double, Ndim> _kp
     // set valence = 1., conduction = 0. initial condition
     // Fermi-Dirac distribution is used when FermiE is applied
     // tempEigval is calculated inside the GenUMatrix, be careful about order or sideeffects.
-    bool isFermiEUsed = false;
+    bool isFermiEUsed = true;
     if (isFermiEUsed)
     {
+        // Version one
+        // for (int m = 0; m < Nband; ++m)
+        // {
+        //     _dmstore[m*Nband + m] = 1.0 / ( exp( (tempEval[m] - FermiE) / thermalE ) + 1.0 );
+        // }
+        
+        // Version two
+        double *tempInitVal = new double[Nband];
+        double tempInitSum = 0;
         for (int m = 0; m < Nband; ++m)
         {
-            _dmstore[m*Nband + m] = 1.0 / ( exp( (tempEval[m] - FermiE) / thermalE ) + 1.0 );
+            tempInitVal[m] = 1.0 / ( exp( (tempEval[m] - FermiE) / thermalE ) + 1.0 );
+            tempInitSum += tempInitVal[m];
         }
+        for (int m = 0; m < Nband; ++m)
+        {
+            _dmstore[m*Nband + m] = tempInitVal[m] / tempInitSum;
+        }
+        delete[] tempInitVal;
     }
     else
     {
@@ -255,56 +281,65 @@ void BieSe3surf::GenJMatrix(complex **_jstore, std::array<double, Ndim> _kpoint)
         cosBvecSum += cos(angle_b0[i]);
         sinBvecSum += sin(angle_b0[i]);
 
-        derxcosAvecSum -= vec_a[i][0] * sin(angle_a0[i]);
+        derxcosAvecSum += -vec_a[i][0] * sin(angle_a0[i]);
         derxsinAvecSum += vec_a[i][0] * cos(angle_a0[i]);
-        derxcosBvecSum -= vec_b[i][0] * sin(angle_b0[i]);
+        derxcosBvecSum += -vec_b[i][0] * sin(angle_b0[i]);
         derxsinBvecSum += vec_b[i][0] * cos(angle_b0[i]);
 
-        derycosAvecSum -= vec_a[i][1] * sin(angle_a0[i]);
+        derycosAvecSum += -vec_a[i][1] * sin(angle_a0[i]);
         derysinAvecSum += vec_a[i][1] * cos(angle_a0[i]);
-        derycosBvecSum -= vec_b[i][1] * sin(angle_b0[i]);
+        derycosBvecSum += -vec_b[i][1] * sin(angle_b0[i]);
         derysinBvecSum += vec_b[i][1] * cos(angle_b0[i]);
     }
 
-    double w = -2*pi/3;
+    double w = -2.*pi/3.;
 
-    xderHcomp[0] = 2*acomp[0] * derxcosAvecSum + 2*bcomp[0] * derxcosBvecSum;
-    xderHcomp[1] = -2*acomp[3] * sin(w)*(vec_a[1][0]*cos(angle_a0[1]) - vec_a[2][0]*cos(angle_a0[2]))
-                    + 2*bcomp[3] * (vec_b[0][0]*cos(angle_b0[0]) 
-                    + cos(w)*(vec_b[1][0]*cos(angle_b0[1]) + vec_b[2][0]*cos(angle_b0[2])) );
-    xderHcomp[2] = -2*bcomp[3] * sin(w)*(vec_b[1][0]*cos(angle_b0[1]) - vec_b[2][0]*cos(angle_b0[2]))
-                    - 2*acomp[3] * (vec_a[0][0]*cos(angle_a0[0]) 
-                    + cos(w)*(vec_a[1][0]*cos(angle_a0[1]) + vec_a[2][0]*cos(angle_a0[2])) );
-    xderHcomp[3] = 2*acomp[2] * derxsinAvecSum;
-    xderHcomp[4] = -2*bcomp[2] * derxsinBvecSum;
-    xderHcomp[5] = 2*acomp[1] * derxcosAvecSum + 2*bcomp[1] * derxcosBvecSum;
+    xderHcomp[0] = 2.*acomp[0] * derxcosAvecSum + 2.*bcomp[0] * derxcosBvecSum;
+    
+    xderHcomp[1] = -2.*acomp[3] * sin(w)*( vec_a[1][0]*cos(angle_a0[1]) - vec_a[2][0]*cos(angle_a0[2]) ) +
+                   + 2.*bcomp[3] * (  vec_b[0][0]*cos(angle_b0[0])
+                   + cos(w)*( vec_b[1][0]*cos(angle_b0[1]) + vec_b[2][0]*cos(angle_b0[2]) ) ) ;
+    
+    
+    xderHcomp[2] = -2.*bcomp[3] * sin( w )*( vec_b[1][0]*cos(angle_b0[1]) - vec_b[2][0]*cos(angle_b0[2]) )
+                    - 2.*acomp[3] * ( vec_a[0][0]*cos(angle_a0[0])
+                     + cos( w )*( vec_a[1][0]*cos(angle_a0[1])
+                                + vec_a[2][0]*cos(angle_a0[2]) )
+                                     );
+    
+    
+    xderHcomp[3] = 2.*acomp[2] * derxsinAvecSum;
+    
+    xderHcomp[4] = -2.*bcomp[2] * derxsinBvecSum;
+    
+    xderHcomp[5] = 2.*acomp[1] * derxcosAvecSum + 2.*bcomp[1] * derxcosBvecSum;
 
-    yderHcomp[0] = 2*acomp[0] * derycosAvecSum + 2*bcomp[0] * derycosBvecSum;
-    yderHcomp[1] = -2*acomp[3] * sin(w)*(vec_a[1][1]*cos(angle_a0[1]) - vec_a[2][1]*cos(angle_a0[2]))
-                    + 2*bcomp[3] * (vec_b[0][1]*cos(angle_b0[0]) 
-                    + cos(w)*(vec_b[1][1]*cos(angle_b0[1]) + vec_b[2][1]*cos(angle_b0[2])) );
-    yderHcomp[2] = -2*bcomp[3] * sin(w)*(vec_b[1][1]*cos(angle_b0[1]) - vec_b[2][1]*cos(angle_b0[2]))
-                    - 2*acomp[3] * (vec_a[0][1]*cos(angle_a0[0]) 
-                    + cos(w)*(vec_a[1][1]*cos(angle_a0[1]) + vec_a[2][1]*cos(angle_a0[2])) );
-    yderHcomp[3] = 2*acomp[2] * derysinAvecSum;
-    yderHcomp[4] = -2*bcomp[2] * derysinBvecSum;
-    yderHcomp[5] = 2*acomp[1] * derycosAvecSum + 2*bcomp[1] * derycosBvecSum;
+    
+    
+    yderHcomp[0] = 2.*acomp[0] * derycosAvecSum + 2.*bcomp[0] * derycosBvecSum;
+    
 
-    // hcomp[0] = 2*acomp[0] * cosAvecSum + 2*bcomp[0] * cosBvecSum;
-    // hcomp[1] = -2*acomp[3] * sin( w*(sin(angle_a0[1]) - sin(angle_a0[2])) ) 
-    //             + 2*bcomp[3] * ( sin(angle_b0[0]) + cos( w*(sin(angle_b0[1]) - sin(angle_b0[2])) ) );
-    // hcomp[2] = -2*bcomp[3] * sin( w*(sin(angle_b0[1]) - sin(angle_b0[2])) )
-    //             -2*acomp[3] * (sin(angle_a0[0]) + cos( w*(sin(angle_a0[1]) + sin(angle_a0[2])) ) );
-    // hcomp[3] = 2*acomp[2] * sinAvecSum;
-    // hcomp[4] = -2*bcomp[2] * sinBvecSum;
-    // hcomp[5] = 2*acomp[1] * cosAvecSum + 2*bcomp[1] * cosBvecSum + m11;
+    
+    yderHcomp[1] = -2.*acomp[3] * sin(w)*( vec_a[1][1]*cos(angle_a0[1]) - vec_a[2][1]*cos(angle_a0[2]) ) +
+    + 2.*bcomp[3] * (  vec_b[0][1]*cos(angle_b0[0])
+                     + cos(w)*(  vec_b[1][1]*cos(angle_b0[1])
+                               + vec_b[2][1]*cos(angle_b0[2]) ) ) ;
+    
+    
+    yderHcomp[2] = -2.*bcomp[3] * sin( w )*( vec_b[1][1]*cos(angle_b0[1]) - vec_b[2][1]*cos(angle_b0[2]) )
+    - 2.*acomp[3] * ( vec_a[0][1]*cos( angle_a0[0] )
+                     + cos( w )*(  vec_a[1][1]*cos(angle_a0[1])
+                                 + vec_a[2][1]*cos(angle_a0[2]) ) );
+    
+    
+    yderHcomp[3] =  2.*acomp[2] * derysinAvecSum;
+    yderHcomp[4] = -2.*bcomp[2] * derysinBvecSum;
+    yderHcomp[5] = 2.*acomp[1] * derycosAvecSum + 2.*bcomp[1] * derycosBvecSum;
 
-    double hcomp5gamma = 6*acomp[1] + 6*bcomp[1] + m11;
+
+    double hcomp5gamma = 6.*(acomp[1] + bcomp[1]) + m11;
     double bcoeff = sqrt(1. - bcomp[0]*bcomp[0]/(bcomp[1]*bcomp[1]));
-    // Bcomp[0] = hcomp[0] + bcomp[0]/bcomp[1] * (-hcomp[5] + hcomp5gamma);
-    // Bcomp[1] = bcoeff * hcomp[1];
-    // Bcomp[2] = bcoeff * hcomp[2];
-    // Bcomp[3] = bcoeff * hcomp[3];
+
 
     xderBcomp[0] = xderHcomp[0] - bcomp[0]/bcomp[1] * xderHcomp[5];
     xderBcomp[1] = bcoeff * xderHcomp[1];
@@ -320,7 +355,89 @@ void BieSe3surf::GenJMatrix(complex **_jstore, std::array<double, Ndim> _kpoint)
     _jstore[0][1] = -(xderBcomp[1] - I*xderBcomp[2]);    _jstore[1][1] = -(yderBcomp[1] - I*yderBcomp[2]);
     _jstore[0][2] = -(xderBcomp[1] + I*xderBcomp[2]);    _jstore[1][2] = -(yderBcomp[1] + I*yderBcomp[2]);
     _jstore[0][3] = -(xderBcomp[0] - xderBcomp[3]);      _jstore[1][3] = -(yderBcomp[0] - yderBcomp[3]);
-    
+
+    // double xderHcomp[5];    double yderHcomp[5];
+    // double xderBcomp[4];    double yderBcomp[4];
+    // fill(xderBcomp, xderBcomp+4, 0.);
+    // fill(yderBcomp, yderBcomp+4, 0.);
+    // fill(xderHcomp, xderHcomp+5, 0.);
+    // fill(yderHcomp, yderHcomp+5, 0.);
+    // double cosAvecSum = 0.;
+    // double sinAvecSum = 0.;
+    // double cosBvecSum = 0.;
+    // double sinBvecSum = 0.;
+
+    // double derxcosAvecSum = 0.;
+    // double derxsinAvecSum = 0.;
+    // double derxcosBvecSum = 0.;
+    // double derxsinBvecSum = 0.;
+    // double derycosAvecSum = 0.;
+    // double derysinAvecSum = 0.;
+    // double derycosBvecSum = 0.;
+    // double derysinBvecSum = 0.;
+    // for (int i = 0; i < 3; ++i)
+    // {
+    //     angle_a0[i] = _kpoint[0]*vec_a[i][0] + _kpoint[1]*vec_a[i][1];
+    //     angle_b0[i] = _kpoint[0]*vec_b[i][0] + _kpoint[1]*vec_b[i][1];
+
+    //     cosAvecSum += cos(angle_a0[i]);
+    //     sinAvecSum += sin(angle_a0[i]);
+    //     cosBvecSum += cos(angle_b0[i]);
+    //     sinBvecSum += sin(angle_b0[i]);
+
+    //     derxcosAvecSum -= vec_a[i][0] * sin(angle_a0[i]);
+    //     derxsinAvecSum += vec_a[i][0] * cos(angle_a0[i]);
+    //     derxcosBvecSum -= vec_b[i][0] * sin(angle_b0[i]);
+    //     derxsinBvecSum += vec_b[i][0] * cos(angle_b0[i]);
+
+    //     derycosAvecSum -= vec_a[i][1] * sin(angle_a0[i]);
+    //     derysinAvecSum += vec_a[i][1] * cos(angle_a0[i]);
+    //     derycosBvecSum -= vec_b[i][1] * sin(angle_b0[i]);
+    //     derysinBvecSum += vec_b[i][1] * cos(angle_b0[i]);
+    // }
+
+    // double w = -2*pi/3;
+
+    // xderHcomp[0] = 2*acomp[0] * derxcosAvecSum + 2*bcomp[0] * derxcosBvecSum;
+    // xderHcomp[1] = -2*acomp[3] * sin(w)*(vec_a[1][0]*cos(angle_a0[1]) - vec_a[2][0]*cos(angle_a0[2]))
+    //                 + 2*bcomp[3] * (vec_b[0][0]*cos(angle_b0[0]) 
+    //                 + cos(w)*(vec_b[1][0]*cos(angle_b0[1]) + vec_b[2][0]*cos(angle_b0[2])) );
+    // xderHcomp[2] = -2*bcomp[3] * sin(w)*(vec_b[1][0]*cos(angle_b0[1]) - vec_b[2][0]*cos(angle_b0[2]))
+    //                 - 2*acomp[3] * (vec_a[0][0]*cos(angle_a0[0]) 
+    //                 + cos(w)*(vec_a[1][0]*cos(angle_a0[1]) + vec_a[2][0]*cos(angle_a0[2])) );
+    // xderHcomp[3] = 2*acomp[2] * derxsinAvecSum;
+    // xderHcomp[4] = -2*bcomp[2] * derxsinBvecSum;
+    // xderHcomp[5] = 2*acomp[1] * derxcosAvecSum + 2*bcomp[1] * derxcosBvecSum;
+
+    // yderHcomp[0] = 2*acomp[0] * derycosAvecSum + 2*bcomp[0] * derycosBvecSum;
+    // yderHcomp[1] = -2*acomp[3] * sin(w)*(vec_a[1][1]*cos(angle_a0[1]) - vec_a[2][1]*cos(angle_a0[2]))
+    //                 + 2*bcomp[3] * (vec_b[0][1]*cos(angle_b0[0]) 
+    //                 + cos(w)*(vec_b[1][1]*cos(angle_b0[1]) + vec_b[2][1]*cos(angle_b0[2])) );
+    // yderHcomp[2] = -2*bcomp[3] * sin(w)*(vec_b[1][1]*cos(angle_b0[1]) - vec_b[2][1]*cos(angle_b0[2]))
+    //                 - 2*acomp[3] * (vec_a[0][1]*cos(angle_a0[0]) 
+    //                 + cos(w)*(vec_a[1][1]*cos(angle_a0[1]) + vec_a[2][1]*cos(angle_a0[2])) );
+    // yderHcomp[3] = 2*acomp[2] * derysinAvecSum;
+    // yderHcomp[4] = -2*bcomp[2] * derysinBvecSum;
+    // yderHcomp[5] = 2*acomp[1] * derycosAvecSum + 2*bcomp[1] * derycosBvecSum;
+
+
+    // double hcomp5gamma = 6*acomp[1] + 6*bcomp[1] + m11;
+    // double bcoeff = sqrt(1. - bcomp[0]*bcomp[0]/(bcomp[1]*bcomp[1]));
+
+    // xderBcomp[0] = xderHcomp[0] - bcomp[0]/bcomp[1] * xderHcomp[5];
+    // xderBcomp[1] = bcoeff * xderHcomp[1];
+    // xderBcomp[2] = bcoeff * xderHcomp[2];
+    // xderBcomp[3] = bcoeff * xderHcomp[3];
+
+    // yderBcomp[0] = yderHcomp[0] - bcomp[0]/bcomp[1] * yderHcomp[5];
+    // yderBcomp[1] = bcoeff * yderHcomp[1];
+    // yderBcomp[2] = bcoeff * yderHcomp[2];
+    // yderBcomp[3] = bcoeff * yderHcomp[3];
+
+    // _jstore[0][0] = -(xderBcomp[0] + xderBcomp[3]);      _jstore[1][0] = -(yderBcomp[0] + yderBcomp[3]);
+    // _jstore[0][1] = -(xderBcomp[1] - I*xderBcomp[2]);    _jstore[1][1] = -(yderBcomp[1] - I*yderBcomp[2]);
+    // _jstore[0][2] = -(xderBcomp[1] + I*xderBcomp[2]);    _jstore[1][2] = -(yderBcomp[1] + I*yderBcomp[2]);
+    // _jstore[0][3] = -(xderBcomp[0] - xderBcomp[3]);      _jstore[1][3] = -(yderBcomp[0] - yderBcomp[3]);
 
 }
 
@@ -359,28 +476,71 @@ void BieSe3surf::GenBcomp(std::array<double, Ndim> _kpoint)
     {
         angle_a0[i] = _kpoint[0]*vec_a[i][0] + _kpoint[1]*vec_a[i][1];
         angle_b0[i] = _kpoint[0]*vec_b[i][0] + _kpoint[1]*vec_b[i][1];
-
+        
         cosAvecSum += cos(angle_a0[i]);
         sinAvecSum += sin(angle_a0[i]);
         cosBvecSum += cos(angle_b0[i]);
         sinBvecSum += sin(angle_b0[i]);
     }
-
-    double w = -2*pi/3;
     
-    hcomp[0] = 2*acomp[0] * cosAvecSum + 2*bcomp[0] * cosBvecSum;
-    hcomp[1] = -2*acomp[3] * sin(w)*(sin(angle_a0[1]) - sin(angle_a0[2]))  
-                + 2*bcomp[3] * (sin(angle_b0[0]) + cos(w)*(sin(angle_b0[1]) - sin(angle_b0[2])) );
-    hcomp[2] = -2*bcomp[3] * sin(w)*(sin(angle_b0[1]) - sin(angle_b0[2])) 
-                - 2*acomp[3] * (sin(angle_a0[0]) + cos(w)*(sin(angle_a0[1]) + sin(angle_a0[2])) );
-    hcomp[3] = 2*acomp[2] * sinAvecSum;
-    hcomp[4] = -2*bcomp[2] * sinBvecSum;
-    hcomp[5] = 2*acomp[1] * cosAvecSum + 2*bcomp[1] * cosBvecSum + m11;
-
-    double hcomp5gamma = 6*acomp[1] + 6*bcomp[1] + m11;
+    double w = -2.*pi/3.;
+    
+    hcomp[0] = 2.*acomp[0] * cosAvecSum + 2.*bcomp[0] * cosBvecSum;
+    
+    hcomp[1] = -2.*acomp[3] * sin( w)*( sin(angle_a0[1]) - sin(angle_a0[2]) )
+    + 2.*bcomp[3] * ( sin(angle_b0[0]) + cos(w)*(sin(angle_b0[1]) - sin(angle_b0[2]) ) ) ;
+    
+    hcomp[2] = -2.*bcomp[3] * sin( w )*( sin(angle_b0[1]) - sin(angle_b0[2]) )
+    -2.*acomp[3] * ( sin(angle_a0[0]) + cos( w )*(sin(angle_a0[1]) + sin(angle_a0[2]) )  );
+    
+    hcomp[3] = 2.*acomp[2] * sinAvecSum;
+    
+    hcomp[4] = -2.*bcomp[2] * sinBvecSum;
+    
+    hcomp[5] = 2.*acomp[1] * cosAvecSum + 2.*bcomp[1] * cosBvecSum + m11;
+    
+    double hcomp5gamma = 6.*acomp[1] + 6.*bcomp[1] + m11;
     double bcoeff = sqrt(1. - bcomp[0]*bcomp[0]/(bcomp[1]*bcomp[1]));
+    
     Bcomp[0] = hcomp[0] + bcomp[0]/bcomp[1] * (-hcomp[5] + hcomp5gamma);
+    
     Bcomp[1] = bcoeff * hcomp[1];
+    
     Bcomp[2] = bcoeff * hcomp[2];
+    
     Bcomp[3] = bcoeff * hcomp[3];
+
+    // fill(Bcomp, Bcomp+4, 0.);
+    // double cosAvecSum = 0.;
+    // double sinAvecSum = 0.;
+    // double cosBvecSum = 0.;
+    // double sinBvecSum = 0.;
+    // for (int i = 0; i < 3; ++i)
+    // {
+    //     angle_a0[i] = _kpoint[0]*vec_a[i][0] + _kpoint[1]*vec_a[i][1];
+    //     angle_b0[i] = _kpoint[0]*vec_b[i][0] + _kpoint[1]*vec_b[i][1];
+
+    //     cosAvecSum += cos(angle_a0[i]);
+    //     sinAvecSum += sin(angle_a0[i]);
+    //     cosBvecSum += cos(angle_b0[i]);
+    //     sinBvecSum += sin(angle_b0[i]);
+    // }
+
+    // double w = -2*pi/3;
+    
+    // hcomp[0] = 2*acomp[0] * cosAvecSum + 2*bcomp[0] * cosBvecSum;
+    // hcomp[1] = -2*acomp[3] * sin(w)*(sin(angle_a0[1]) - sin(angle_a0[2]))  
+    //             + 2*bcomp[3] * (sin(angle_b0[0]) + cos(w)*(sin(angle_b0[1]) - sin(angle_b0[2])) );
+    // hcomp[2] = -2*bcomp[3] * sin(w)*(sin(angle_b0[1]) - sin(angle_b0[2])) 
+    //             - 2*acomp[3] * (sin(angle_a0[0]) + cos(w)*(sin(angle_a0[1]) + sin(angle_a0[2])) );
+    // hcomp[3] = 2*acomp[2] * sinAvecSum;
+    // hcomp[4] = -2*bcomp[2] * sinBvecSum;
+    // hcomp[5] = 2*acomp[1] * cosAvecSum + 2*bcomp[1] * cosBvecSum + m11;
+
+    // double hcomp5gamma = 6*acomp[1] + 6*bcomp[1] + m11;
+    // double bcoeff = sqrt(1. - bcomp[0]*bcomp[0]/(bcomp[1]*bcomp[1]));
+    // Bcomp[0] = hcomp[0] + bcomp[0]/bcomp[1] * (-hcomp[5] + hcomp5gamma);
+    // Bcomp[1] = bcoeff * hcomp[1];
+    // Bcomp[2] = bcoeff * hcomp[2];
+    // Bcomp[3] = bcoeff * hcomp[3];
 }
