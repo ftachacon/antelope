82255161fa2975c778030b6fd51ce08a53d9477c
Dasol <dsestil@gmail.com>
2020-12-31 15:41:42 +0900

    Revert slurm script change - no OpenMp combination

--- src/WannierGauge/LengthGauge/MovingFrame/GeneralModel/submit_slurm.sh
+++ src/WannierGauge/LengthGauge/MovingFrame/GeneralModel/submit_slurm.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 #Number of MPI tasks
-#SBATCH -n 2
+#SBATCH -n 23
 
-#Number of cores per task
-#SBATCH -c 11
 #SBATCH -J "Wannier"
 
 time -p mpirun -np $SLURM_NTASKS ./exec_hhg_mpi 1> stdout.log 2> stderr.log
