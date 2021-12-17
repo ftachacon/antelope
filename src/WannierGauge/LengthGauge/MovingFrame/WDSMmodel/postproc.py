bf1c13ebe658441047cb5a90419c050866b6d49e
geonda <areyougeonda@gmail.com>
2021-12-17 12:33:03 +0300

    update postproc

--- src/WannierGauge/LengthGauge/MovingFrame/WDSMmodel/postproc.py
+++ src/WannierGauge/LengthGauge/MovingFrame/WDSMmodel/postproc.py
@@ -29,16 +29,17 @@ class postprocessing(object):
         self.spectrum_ymax = 1
         self.parse_input()
         self.parse_laser()
+        self.color_list = ['b', 'r', 'g']
         # self.current_processing()
 
     def plot_t_data(self, data_list=[], label_list=[], save=False, figname=''):
         fig = plt.figure(figsize=(self.width, self.hight))
-        for item, label in zip(data_list, label_list):
-            plt.plot(self.t, item, lw=2., label=label)
-        plt.legend()
-        plt.xlabel('time (a.u.) ', fontsize=18)
-        plt.ylabel('$J$ (a.u.) ', fontsize=18)
-        plt.tick_params(labelsize=18)
+        for item, label, color in zip(data_list, label_list, self.color_list):
+            plt.plot(self.t, item, lw=2., label=label, color=color)
+        plt.legend(fontsize=30)
+        plt.xlabel('time (a.u.) ', fontsize=30)
+        plt.ylabel('$J$ (a.u.) ', fontsize=30)
+        plt.tick_params(labelsize=20)
         xaxmin = self.t.min()     #
         xaxmax = self.t.max()     #
         plt.xlim(xaxmin, xaxmax)
@@ -50,7 +51,8 @@ class postprocessing(object):
         fft_current = np.zeros(data.shape, dtype=np.complex128)
         for i in range(self.dim):
             fft_current[i, :] = fft(data[i, :]) * self.dt
-            fft_current[i, :] = -1j * self.w * np.fft.fftshift(fft_current[i, :])
+            fft_current[i, :] = -1j * self.w * \
+                np.fft.fftshift(fft_current[i, :])
         self.fft_current = fft_current
         return self.fft_current
 
@@ -67,40 +69,33 @@ class postprocessing(object):
                            + np.cos(thetaz) * data[2, :])
         self.radiation = radiation
         self.spectrum = abs(
-            self.radiation[0, :])**2 + abs(self.radiation[1, :])**2 + abs(self.radiation[2, :])**2
-
-    def plot_w_data(self, data_list=[], label_list=[], save=False, figname=''):
-        fig = plt.figure(figsize=(self.width, self.hight))
-        for item, label in zip(data_list, label_list):
-            item = np.log10(item)
-            plt.plot(self.w / self.w0, item, lw=2., label=label)
-            plt.ylim(min(item), max(item))
-        plt.xlabel(r'$\rm Harmonic-Order$', fontsize=30)
-        plt.ylabel(r'$\rm Log_{10}(I_{HHG})$', fontsize=30)
-        plt.tick_params(labelsize=20)
-        plt.grid(True)
-        plt.xlim(self.spectrum_xmin, self.spectrum_xmax)
-
-        plt.legend(fontsize=18)
-
-    def parse_laser(self):
-        self.laser = np.transpose(np.loadtxt(
-            '{}/outlaserdata.dat'.format(self.project_path)))
-        self.t = self.laser[0, :]
-        self.Efield = self.laser[1:3, :]
-        self.Nt = len(self.t)
-        self.dt = self.t[1] - self.t[0]
-        self.wmax = np.pi / self.dt
-        self.wmin = -self.wmax
-        self.dw = 2. * self.wmax / float(self.Nt)
-        self.w = np.linspace(self.wmin, self.wmax - self.dw, num=self.Nt)
+            self.radiation[0, :])**2 + abs(self.radiation[1, :])**2 + abs(self.radiation[2, :])**2 + self.hzero
+        self.spectrum_x = abs(self.radiation[0, :])**2 + self.hzero
+        self.spectrum_y = abs(self.radiation[1, :])**2 + self.hzero
+        self.spectrum_z = abs(self.radiation[2, :])**2 + self.hzero
 
     def current_processing(self, data):
         data_masked = np.zeros(data.shape, dtype=np.float64)
         for i in range(self.dim):
             data_masked[i, :] = np.blackman(self.Nt) * data[i, :]
         self.rotation(self.fft(data_masked))
-        return self.spectrum
+        return self.spectrum, self.spectrum_x, self.spectrum_y
+
+    def plot_w_data(self, data_list=[], label_list=[], save=False, figname=''):
+        fig = plt.figure(figsize=(self.width, self.hight))
+        for item, label, color in zip(data_list, label_list, self.color_list):
+            # item = np.log10(item)
+            plt.plot(self.w / self.w0, item, lw=2., label=label, color=color)
+            # plt.fill(self.w / self.w0, item, alpha=0.1, color=color)
+            plt.ylim(min(item), 1)
+        plt.yscale('log')
+        plt.xlabel(r'$\rm Harmonic-Order$', fontsize=30)
+        plt.ylabel(r'$\rm I_{HHG}, arb. units$', fontsize=30)
+        plt.tick_params(labelsize=20)
+        plt.grid(True)
+        plt.xlim(self.spectrum_xmin, self.spectrum_xmax)
+
+        plt.legend(fontsize=18)
 
     def parse_input(self):
         with io.open(self.project_path + '/inputParam.cfg') as f:
@@ -120,3 +115,30 @@ class postprocessing(object):
                   fr"phix = {self.config['laser']['pulses'][i]['phix']} rad, thetaz = {self.config['laser']['pulses'][i]['thetaz']} rad, "
                   fr"phiz = {self.config['laser']['pulses'][i]['phiz']} rad, envelope = {self.config['laser']['pulses'][i]['env_name']}")
         print("==============================")
+
+    def parse_laser(self):
+        self.laser = np.transpose(np.loadtxt(
+            '{}/outlaserdata.dat'.format(self.project_path)))
+        self.t = self.laser[0, :]
+        self.Efield = self.laser[1:3, :]
+        self.Nt = len(self.t)
+        self.dt = self.t[1] - self.t[0]
+        self.wmax = np.pi / self.dt
+        self.wmin = -self.wmax
+        self.dw = 2. * self.wmax / float(self.Nt)
+        self.w = np.linspace(self.wmin, self.wmax - self.dw, num=self.Nt)
+
+    def plot_laser(self, save=False, savefig=''):
+        fig = plt.figure(figsize=(self.width, self.hight))
+        plt.plot(self.t, self.Efield[0, :], lw=2., color='b', label='$E_x$')
+        plt.plot(self.t, self.Efield[1, :], lw=2., color='g', label='$E_y$')
+        plt.legend()
+        plt.xlabel('time (a.u.) ', fontsize=18)
+        plt.ylabel('$E$ (a.u.) ', fontsize=18)
+        plt.tick_params(labelsize=18)
+        xaxmin = self.t.min()     #
+        xaxmax = self.t.max()     #
+        plt.xlim(xaxmin, xaxmax)
+        plt.tight_layout()
+        if save:
+            plt.savefig(figname)
