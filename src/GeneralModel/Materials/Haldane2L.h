8aefd6334b2906afa5e38d9469c266108d90d490
Dasol <dsestil@gmail.com>
2022-07-15 02:52:11 +0900

    Haldane2L improved

--- src/GeneralModel/Materials/Haldane2L.h
+++ src/GeneralModel/Materials/Haldane2L.h
@@ -15,10 +15,11 @@ class Haldane2L : public BaseMaterial
 {
 public:
     // Haldnae parameters
+    // layer top (0) and bottom (1) have seprate parameters. Except a0 (it should be implemented as a seprate model.)
     // t11 - interlayer orthogonal param (hopping between directly above and below atoms)
-    double t1, t2, M0, phi0, a0, t11; 
+    double t1[2], t2[2], M0[2], phi0[2], a0, t11; 
     complex h_inter[4];
-    double B3sign[2];     // B3 sign for each layer
+    double B2sign[2];     // B2 sign for each layer (-1 for complex conjugate)
     double vec_a[3][2];          ///< 3  NN vectors (from A to B )
     double vec_b[3][2];          ///< 3 NNN vectors (from A to A, or B to B)
 
@@ -26,6 +27,7 @@ public:
 
     // internal variables for storing temporary values
     double Bcomp[4];
+    double Bcomp_bottom[4];
     double angle_a0, angle_b0;
 
     std::array<double, 9> BZaxis;
@@ -33,6 +35,8 @@ public:
 
     // polytype - 2H or 3R
     std::string polytype;
+    // check whether top and bottom have same paramters.
+    bool is_top_bottom_same;
 
     Haldane2L( const libconfig::Setting *params );
     void SetBasis();
@@ -49,26 +53,48 @@ public:
 Haldane2L::Haldane2L( const libconfig::Setting *params )
 {
     Nband = 4;  Nval = 2;   isDipoleZero = true;
-    if (params->lookupValue("t1", t1)
-        && params->lookupValue("t2", t2)
+    if (params->lookupValue("t1", t1[0])
+        && params->lookupValue("t2", t2[0])
         //&& params->lookupValue("Mt2", M0)
-        && params->lookupValue("phi0", phi0)
+        && params->lookupValue("phi0", phi0[0])
         && params->lookupValue("a0", a0)
         && params->lookupValue("t11", t11)
         && params->lookupValue("polytype", polytype) )
     {
         a0 /= au_angstrom;
-        if (!params->lookupValue("M0", M0))
+        if (!params->lookupValue("M0", M0[0]))
         {
-            if (params->lookupValue("Mt2",M0))
+            if (params->lookupValue("Mt2",M0[0]))
             {
-                M0 *= t2;
+                M0[0] *= t2[0];
             }
             else
             {
                 std::cerr << "M0 param missing" << std::endl;
                 std::exit(EXIT_FAILURE);
             }
+        }
+
+        // bottom layer paramters default
+        is_top_bottom_same = true;
+        t1[1] = t1[0];  t2[1] = t2[0];
+        phi0[1] = phi0[0]; M0[1] = M0[0];
+
+        // bottom layer paramters
+        if (params->lookupValue("t1_bottom", t1[1])
+            || params->lookupValue("t2_bottom", t2[1])
+            || params->lookupValue("phi0_bottom", phi0[1]))
+        {
+            is_top_bottom_same = false;
+        }
+        if (params->lookupValue("M0_bottom", M0[1]))
+        {
+            is_top_bottom_same = false;
+        }
+        else if (params->lookupValue("Mt2_bottom",M0[1]))
+        {
+            M0[1] *= t2[1];
+            is_top_bottom_same = false;
         }
     }
     else
@@ -77,47 +103,43 @@ Haldane2L::Haldane2L( const libconfig::Setting *params )
         std::exit(EXIT_FAILURE);
     }
 
+    // defulat B2 sign
+    B2sign[0] = 1.;  B2sign[1] = 1.;
+
+    // Notation: A, B position in matrix is unchanged - Atop, Btop, Abottom, Bbottom
+    // Stacking follows PhysRevB.83.235312 (ribeiro2011)
     if (polytype == "AA")
     {
         h_inter[0] = t11;   h_inter[1] = 0.;
         h_inter[2] = 0.;    h_inter[3] = t11;
-        B3sign[0] = 1.;
-        B3sign[1] = 1.;
     }
     else if (polytype == "AB" || polytype == "AB1")
     {
         h_inter[0] = 0.;    h_inter[1] = t11;
         h_inter[2] = 0.;    h_inter[3] = 0.;
-        B3sign[0] = 1.;
-        B3sign[1] = 1.;
     }
     else if (polytype == "AB2" || polytype == "3R")
     {
-        h_inter[0] = 0.;    h_inter[1] = 0.;
+        h_inter[0] = 0.;     h_inter[1] = 0.;
         h_inter[2] = t11;    h_inter[3] = 0.;
-        B3sign[0] = 1.;
-        B3sign[1] = 1.;
     }
     else if (polytype == "AA'" || polytype == "2H")
     {
-        h_inter[0] = t11;   h_inter[1] = 0.;
-        h_inter[2] = 0.;    h_inter[3] = t11;
-        B3sign[0] = 1.;
-        B3sign[1] = -1.;
+        h_inter[0] = 0.;     h_inter[1] = t11;
+        h_inter[2] = t11;    h_inter[3] = 0.;
+        B2sign[1] = -1.;
     }
     else if (polytype == "A'B")
     {
-        h_inter[0] = 0.;    h_inter[1] = t11;
-        h_inter[2] = 0.;    h_inter[3] = 0.;
-        B3sign[0] = -1.;
-        B3sign[1] = 1.;
+        h_inter[0] = 0.;    h_inter[1] = 0.;
+        h_inter[2] = 0.;    h_inter[3] = t11;
+        B2sign[0] = -1.;
     }
     else if (polytype == "AB'")
     {
-        h_inter[0] = 0.;    h_inter[1] = t11;
-        h_inter[2] = 0.;    h_inter[3] = 0.;
-        B3sign[0] = 1.;
-        B3sign[1] = -1.;
+        h_inter[0] = t11;    h_inter[1] = 0.;
+        h_inter[2] = 0.;     h_inter[3] = 0.;
+        B2sign[1] = -1.;
     }
     else
     {
@@ -147,33 +169,32 @@ void Haldane2L::GenHamiltonian(complex *_hstore, std::array<double, Ndim> _kpoin
     GenBcomp(_kpoint);
 
     // block diagonal part (intra-layer part)
-    _hstore[0] = Bcomp[0] + B3sign[0]*Bcomp[3];     _hstore[1] = Bcomp[1] - I*Bcomp[2];
-    _hstore[4] = Bcomp[1] + I*Bcomp[2];   _hstore[5] = Bcomp[0] - B3sign[0]*Bcomp[3];
+    _hstore[0] = Bcomp[0] + Bcomp[3];               _hstore[1] = Bcomp[1] - B2sign[0]*I*Bcomp[2];
+    _hstore[4] = Bcomp[1] + B2sign[0]*I*Bcomp[2];   _hstore[5] = Bcomp[0] - Bcomp[3];
 
-    _hstore[10] = Bcomp[0] + B3sign[1]*Bcomp[3];     _hstore[11] = Bcomp[1] - I*Bcomp[2];
-    _hstore[14] = Bcomp[1] + I*Bcomp[2];              _hstore[15] = Bcomp[0] - B3sign[1]*Bcomp[3];
+    _hstore[10] = Bcomp_bottom[0] + Bcomp_bottom[3];              _hstore[11] = Bcomp_bottom[1] - B2sign[1]*I*Bcomp_bottom[2];
+    _hstore[14] = Bcomp_bottom[1] + B2sign[1]*I*Bcomp_bottom[2];  _hstore[15] = Bcomp_bottom[0] - Bcomp_bottom[3];
 
     // inter-layer interaction part
-    // add conjugate later if you want to include complex interaction term
     _hstore[2] = h_inter[0];    _hstore[3] = h_inter[1];
     _hstore[6] = h_inter[2];    _hstore[7] = h_inter[3];
 
-    _hstore[8] = h_inter[0];    _hstore[9] = h_inter[2];
-    _hstore[12] = h_inter[1];   _hstore[13] = h_inter[3];
+    _hstore[8] = conj(h_inter[0]);    _hstore[9] = conj(h_inter[2]);
+    _hstore[12] = conj(h_inter[1]);   _hstore[13] = conj(h_inter[3]);
 }
 
 void Haldane2L::GenJMatrix(complex **_jstore, std::array<double, Ndim> _kpoint)
 {
-    double xderBcomp[4];    double yderBcomp[4];
-    std::fill(Bcomp, Bcomp+4, 0.);
-    std::fill(xderBcomp, xderBcomp+4, 0.);
-    std::fill(yderBcomp, yderBcomp+4, 0.);
+    double xderBcomp[4] = {0};    double yderBcomp[4] = {0};
+    //std::fill(Bcomp, Bcomp+4, 0.);
+    // std::fill(xderBcomp, xderBcomp+4, 0.);
+    // std::fill(yderBcomp, yderBcomp+4, 0.);
     for (int i = 0; i < 3; ++i)
     {
         angle_a0 = _kpoint[0]*vec_a[i][0] + _kpoint[1]*vec_a[i][1];
         angle_b0 = _kpoint[0]*vec_b[i][0] + _kpoint[1]*vec_b[i][1];
-        Bcomp[0] += cos( angle_b0 );    Bcomp[1] += cos( angle_a0 );
-        Bcomp[2] += sin( angle_a0 );    Bcomp[3] += sin( angle_b0 );
+        // Bcomp[0] += cos( angle_b0 );    Bcomp[1] += cos( angle_a0 );
+        // Bcomp[2] += sin( angle_a0 );    Bcomp[3] += sin( angle_b0 );
 
         xderBcomp[0] += vec_b[i][0] * sin( angle_b0 );      yderBcomp[0] += vec_b[i][1] * sin( angle_b0 );
         xderBcomp[1] += vec_a[i][0] * sin( angle_a0 );      yderBcomp[1] += vec_a[i][1] * sin( angle_a0 );
@@ -181,23 +202,44 @@ void Haldane2L::GenJMatrix(complex **_jstore, std::array<double, Ndim> _kpoint)
         xderBcomp[3] += vec_b[i][0] * cos( angle_b0 );      yderBcomp[3] += vec_b[i][1] * cos( angle_b0 );
     }
 
-    Bcomp[0] *= 2.* t2 * cos( phi0 );       Bcomp[1] *= t1;
-    Bcomp[2] *= t1;                         Bcomp[3] = M0 - 2.*t2*sin( phi0 )*Bcomp[3];
+    // Bcomp[0] *= 2.* t2[0] * cos( phi0[0] );    Bcomp[1] *= t1[0];
+    // Bcomp[2] *= t1[0];                         Bcomp[3] = M0[0] - 2.*t2[0]*sin( phi0[0] )*Bcomp[3];
 
-    xderBcomp[0] *= -2.*t2 * cos(phi0);     yderBcomp[0] *= -2.*t2 * cos(phi0);
-    xderBcomp[1] *= -t1;                    yderBcomp[1] *= -t1;
-    xderBcomp[2] *= t1;                     yderBcomp[2] *= t1;
-    xderBcomp[3] *= -2.*t2 * sin(phi0);     yderBcomp[3] *= -2.*t2 * sin(phi0);
+    xderBcomp[0] *= -2.*t2[0] * cos(phi0[0]);  yderBcomp[0] *= -2.*t2[0] * cos(phi0[0]);
+    xderBcomp[1] *= -t1[0];                    yderBcomp[1] *= -t1[0];
+    xderBcomp[2] *= t1[0];                     yderBcomp[2] *= t1[0];
+    xderBcomp[3] *= -2.*t2[0] * sin(phi0[0]);  yderBcomp[3] *= -2.*t2[0] * sin(phi0[0]);
 
-    _jstore[0][0] = -(xderBcomp[0] + B3sign[0]*xderBcomp[3]);      _jstore[1][0] = -(yderBcomp[0] + B3sign[0]*yderBcomp[3]);
-    _jstore[0][1] = -(xderBcomp[1] - I*xderBcomp[2]);              _jstore[1][1] = -(yderBcomp[1] - I*yderBcomp[2]);
-    _jstore[0][4] = -(xderBcomp[1] + I*xderBcomp[2]);              _jstore[1][4] = -(yderBcomp[1] + I*yderBcomp[2]);
-    _jstore[0][5] = -(xderBcomp[0] - B3sign[0]*xderBcomp[3]);      _jstore[1][5] = -(yderBcomp[0] - B3sign[0]*yderBcomp[3]);
+    _jstore[0][0] = -(xderBcomp[0] + xderBcomp[3]);               _jstore[1][0] = -(yderBcomp[0] + yderBcomp[3]);
+    _jstore[0][1] = -(xderBcomp[1] - B2sign[0]*I*xderBcomp[2]);   _jstore[1][1] = -(yderBcomp[1] - B2sign[0]*I*yderBcomp[2]);
+    _jstore[0][4] = -(xderBcomp[1] + B2sign[0]*I*xderBcomp[2]);   _jstore[1][4] = -(yderBcomp[1] + B2sign[0]*I*yderBcomp[2]);
+    _jstore[0][5] = -(xderBcomp[0] - xderBcomp[3]);               _jstore[1][5] = -(yderBcomp[0] - yderBcomp[3]);
 
-    _jstore[0][10] = -(xderBcomp[0] + B3sign[1]*xderBcomp[3]);      _jstore[1][10] = -(yderBcomp[0] + B3sign[1]*yderBcomp[3]);
-    _jstore[0][11] = -(xderBcomp[1] - I*xderBcomp[2]);              _jstore[1][11] = -(yderBcomp[1] - I*yderBcomp[2]);
-    _jstore[0][14] = -(xderBcomp[1] + I*xderBcomp[2]);              _jstore[1][14] = -(yderBcomp[1] + I*yderBcomp[2]);
-    _jstore[0][15] = -(xderBcomp[0] - B3sign[1]*xderBcomp[3]);      _jstore[1][15] = -(yderBcomp[0] - B3sign[1]*yderBcomp[3]);
+
+    // bottom layer
+    //std::fill(Bcomp, Bcomp+4, 0.);
+    std::fill(xderBcomp, xderBcomp+4, 0.);
+    std::fill(yderBcomp, yderBcomp+4, 0.);
+    for (int i = 0; i < 3; ++i)
+    {
+        angle_a0 = _kpoint[0]*vec_a[i][0] + _kpoint[1]*vec_a[i][1];
+        angle_b0 = _kpoint[0]*vec_b[i][0] + _kpoint[1]*vec_b[i][1];
+
+        xderBcomp[0] += vec_b[i][0] * sin( angle_b0 );      yderBcomp[0] += vec_b[i][1] * sin( angle_b0 );
+        xderBcomp[1] += vec_a[i][0] * sin( angle_a0 );      yderBcomp[1] += vec_a[i][1] * sin( angle_a0 );
+        xderBcomp[2] += vec_a[i][0] * cos( angle_a0 );      yderBcomp[2] += vec_a[i][1] * cos( angle_a0 );
+        xderBcomp[3] += vec_b[i][0] * cos( angle_b0 );      yderBcomp[3] += vec_b[i][1] * cos( angle_b0 );
+    }
+
+    xderBcomp[0] *= -2.*t2[1] * cos(phi0[1]);  yderBcomp[0] *= -2.*t2[1] * cos(phi0[1]);
+    xderBcomp[1] *= -t1[1];                    yderBcomp[1] *= -t1[1];
+    xderBcomp[2] *= t1[1];                     yderBcomp[2] *= t1[1];
+    xderBcomp[3] *= -2.*t2[1] * sin(phi0[1]);  yderBcomp[3] *= -2.*t2[1] * sin(phi0[1]);
+
+    _jstore[0][10] = -(xderBcomp[0] + xderBcomp[3]);               _jstore[1][10] = -(yderBcomp[0] + yderBcomp[3]);
+    _jstore[0][11] = -(xderBcomp[1] - B2sign[1]*I*xderBcomp[2]);   _jstore[1][11] = -(yderBcomp[1] - B2sign[1]*I*yderBcomp[2]);
+    _jstore[0][14] = -(xderBcomp[1] + B2sign[1]*I*xderBcomp[2]);   _jstore[1][14] = -(yderBcomp[1] + B2sign[1]*I*yderBcomp[2]);
+    _jstore[0][15] = -(xderBcomp[0] - xderBcomp[3]);               _jstore[1][15] = -(yderBcomp[0] - yderBcomp[3]);
     
 
 }
@@ -223,11 +265,11 @@ void Haldane2L::PrintMaterialInformation()
     std::cout << "============================================\n";
     std::cout << "Haldane2L Model paramters\n";
     std::cout << "a0         = " << a0 << " a.u. \n";
-    std::cout << "t1         = " << t1 << " a.u. \n";
-    std::cout << "t2         = " << t2 << " a.u. \n";
-    std::cout << "M0         = " << M0 << " a.u. \n";
-    std::cout << "B3 sign    = " << B3sign[0] << ", " << B3sign[1] << " \n";
-    std::cout << "phi0       = " << phi0 << " rad \n";
+    std::cout << "t1         = " << t1[0] << ", " << t1[1] << " a.u. \n";
+    std::cout << "t2         = " << t2[0] << ", " << t2[1] << " a.u. \n";
+    std::cout << "M0         = " << M0[0] << ", " << M0[1] << " a.u. \n";
+    std::cout << "B2 sign    = " << B2sign[0] << ", " << B2sign[1] << " \n";
+    std::cout << "phi0       = " << phi0[0] << ", " << phi0[1] << " rad \n";
     std::cout << "t11        = " << t11 << " a.u. \n";
     std::cout << "polytype   = " << polytype << "\n";
     std::cout << "============================================\n";
@@ -244,6 +286,26 @@ void Haldane2L::GenBcomp(std::array<double, Ndim> _kpoint)
         Bcomp[2] += sin( angle_a0 );    Bcomp[3] += sin( angle_b0 );
     }
 
-    Bcomp[0] *= 2.* t2 * cos( phi0 );       Bcomp[1] *= t1;
-    Bcomp[2] *= t1;                         Bcomp[3] = M0 - 2.*t2*sin( phi0 )*Bcomp[3];
+    Bcomp[0] *= 2.* t2[0] * cos( phi0[0] );       Bcomp[1] *= t1[0];
+    Bcomp[2] *= t1[0];                            Bcomp[3] = M0[0] - 2.*t2[0]*sin( phi0[0] )*Bcomp[3];
+
+    if (is_top_bottom_same)
+    {
+        Bcomp_bottom[0] = Bcomp[0];    Bcomp_bottom[1] = Bcomp[1];
+        Bcomp_bottom[2] = Bcomp[2];    Bcomp_bottom[3] = Bcomp[3];
+    }
+    else
+    {
+        std::fill(Bcomp_bottom, Bcomp_bottom+4, 0.);
+        for (int i = 0; i < 3; ++i)
+        {
+            angle_a0 = _kpoint[0]*vec_a[i][0] + _kpoint[1]*vec_a[i][1];
+            angle_b0 = _kpoint[0]*vec_b[i][0] + _kpoint[1]*vec_b[i][1];
+            Bcomp_bottom[0] += cos( angle_b0 );    Bcomp_bottom[1] += cos( angle_a0 );
+            Bcomp_bottom[2] += sin( angle_a0 );    Bcomp_bottom[3] += sin( angle_b0 );
+        }
+
+        Bcomp_bottom[0] *= 2.* t2[1] * cos( phi0[1] );       Bcomp_bottom[1] *= t1[1];
+        Bcomp_bottom[2] *= t1[1];                            Bcomp_bottom[3] = M0[1] - 2.*t2[1]*sin( phi0[1] )*Bcomp[3];
+    }
 }
